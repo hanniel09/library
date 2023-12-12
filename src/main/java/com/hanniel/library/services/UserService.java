a7e484f7e582bf956c3e43ecffa6d0330eff91a9
hanniel09 <hannielvieira1227@gmail.com>
2023-12-12 13:46:11 -0300

    fact: update UserService

--- src/main/java/com/hanniel/library/services/UserService.java
+++ src/main/java/com/hanniel/library/services/UserService.java
@@ -1,8 +1,15 @@
 package com.hanniel.library.services;
 
+import com.hanniel.library.dtos.UserDTO;
+import com.hanniel.library.models.UserModel;
 import com.hanniel.library.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -10,7 +17,15 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
-    Public User createUser(){
+    public List<UserModel> getUser(){
+        return userRepository.findAll();
+    }
 
+    public UserModel createUser(UserDTO data){
+        UserModel user = new UserModel(data);
+        BCryptPasswordEncoder bcrypt = new BCryptPasswordEncoder();
+        String passwordCrypt = bcrypt.encode(user.getPassword());
+        user.setPassword(passwordCrypt);
+        return userRepository.save(user);
     }
 }
